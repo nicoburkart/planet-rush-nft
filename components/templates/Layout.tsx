9c06ff04283bdd42a2d2654dbf181734707ba1fe
nicoburkart <nicoburkart@web.de>
2022-04-28 21:50:54 +0200

    feat: 💬 changed title to planetrush

--- components/templates/Layout.tsx
+++ components/templates/Layout.tsx
@@ -9,7 +9,7 @@ type Props = {
   title?: string;
 };
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'PlanetRush' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
